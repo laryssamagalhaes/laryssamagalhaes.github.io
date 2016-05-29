a8b8bcc5e538dcec373484f1fc6d6234c3d64354
Laryssa Magalhães <Laryssa Magalhes>
2016-05-28 21:41:56 -0300

    configurações iniciais

---
+++ gulpfile.js
@@ -0,0 +1,45 @@
+var gulp = require('gulp');
+var plumber = require('gulp-plumber');
+var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+var cssnano = require('gulp-cssnano');
+var cssglobbing = require('gulp-css-globbing');
+var autoprefixer = require('gulp-autoprefixer');
+var cmq = require('gulp-merge-media-queries');
+var browserSync = require('browser-sync').create();
+
+gulp.task('default', ['sass', 'browser-sync', 'watch'], function(){});
+
+gulp.task('sass', function () {
+  return gulp.src('./app/assets/sass/main.scss')
+  	.pipe(plumber())
+  	.pipe(cssglobbing({
+  		extensions: ['.scss']
+  	}))
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer({
+    	browsers: ['not ie <= 8'],
+    	cascade: false
+    }))
+    .pipe(concat('main.css'))
+    .pipe(cmq())
+    .pipe(cssnano())
+    .pipe(gulp.dest('./assets/css'));
+});
+
+
+gulp.task('watch', function () {
+  gulp.watch('./app/assets/sass/**/*.scss', ['sass', 'refresh']);
+});
+
+gulp.task('browser-sync', function () {
+  	browserSync.init({
+    	server: {
+      		baseDir: './'
+    	}
+  	});
+});
+
+gulp.task('refresh', function () {
+  	browserSync.reload();
+});
